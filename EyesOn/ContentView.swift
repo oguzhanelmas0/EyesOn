235d81ac8cd088b581154cec3c04b4266ee37e33
oguzhanelmas0 <elmasog@mef.edu.tr>
2026-05-03 14:27:28 +0300

    Initial Commit

---
+++ EyesOn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EyesOn
+//
+//  Created by Oğuzhan Elmas on 3.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EyesOn/EyesOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyesOnApp.swift
+//  EyesOn
+//
+//  Created by Oğuzhan Elmas on 3.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct EyesOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
